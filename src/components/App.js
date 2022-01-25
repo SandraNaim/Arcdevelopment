177621f1ae27011b11332dda1d7ae2a3e2ce4099
Sandra Naim <naimsandra9@gmail.com>
2022-01-25 13:57:10 +0200

    add assets file with all the images needed in the project, create a header component and write the suitable theme properties

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+
+import theme from  "./ui/Theme.js";
+import Header from "./ui/Header";
+
+function App() {
+  return (
+    <MuiThemeProvider theme={theme}>
+      <Header />
+      Hello!
+    </MuiThemeProvider>
+  );
+}
+
+export default App;
