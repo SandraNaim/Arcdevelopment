1f1c5f8876cc47c7f32ddd8161c783b0711dacac
Sandra Naim <naimsandra9@gmail.com>
2022-01-29 18:21:32 +0200

    build the custom software section by adding the icon and its text on row grid

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Lottie from "react-lottie";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import ButtonArrow from "../components/ui/ButtonArrow";
 
 import animationData from "../animations/landinganimation/data";
-
+import customSoftwareIcon from "../assets/CustomSoftwareIcon.svg"
 const useStyles = makeStyles(theme => ({
   animation: {
     maxWidth: "40em",
@@ -33,16 +34,19 @@ const useStyles = makeStyles(theme => ({
     marginTop: "1em"
   },
   learnButtonHero: {
-    borderColor: theme.palette.common.blue,
-    color: theme.palette.common.blue,
-    borderWidth: 2,
-    textTransform: "none",
-    borderRadius: 50,
-    fontFamily: "Roboto",
-    fontWeight: "bold",
+    ...theme.typography.learnButton,
     fontSize: "0.9rem",
     height: 45,
     width: 145,
+  },
+  learnButtonServices: {
+    ...theme.typography.learnButton,
+    fontSize: "0.7rem",
+    height: 35,
+    padding: 5,
+    [theme.breakpoints.down("sm")]: {
+      marginBottom: "2em"
+    }
   },
   mainContainer: {
     marginTop: "5em",
@@ -59,12 +63,32 @@ const useStyles = makeStyles(theme => ({
     [theme.breakpoints.down("xs")]: {
       marginLeft: "0"
     },
+  },
+  specialFont: {
+    fontFamily:"Pacifico",
+    color: theme.palette.common.orange
+  },
+  subtitle: {
+    marginBottom: "1em"
+  },
+  customSoftwareIcon: {
+    marginLeft: "2em",
+    [theme.breakpoints.down("md")]: {
+      marginLeft: 0
+    }
+  },
+  serviceContainer: {
+    marginTop: "12em",
+    [theme.breakpoints.down("sm")]: {
+      padding: 25
+    }
   }
 }))
 
 export default function LandingPage() {
     const classes = useStyles();
     const theme = useTheme();
+    const matchesSM = useMediaQuery(theme.breakpoints.down("sm"));
 
     const defaultOptions = {
         loop: true,
@@ -77,6 +101,7 @@ export default function LandingPage() {
 
     return (
       <Grid container direction="column" className={classes.mainContainer}>
+        {/* ____Hero Block____ */}
         <Grid item>
           <Grid container direction="row" justify="flex-end" alignItems="center" >
             <Grid item sm className={classes.heroTextContainer}>
@@ -97,6 +122,31 @@ export default function LandingPage() {
             </Grid>
             <Grid item sm className={classes.animation}>
               <Lottie options={defaultOptions} height={"100%"} width={"100%"} />
+            </Grid>
+          </Grid>
+        </Grid>
+
+        {/* ___Services Block___ */}
+        <Grid item>
+          <Grid container direction="row" justify={matchesSM ? "center" : "flex-start"} className={classes.serviceContainer}>
+            <Grid item style={{marginLeft: matchesSM ? 0 : "5em", textAlign: matchesSM ? "center" : undefined}}>
+              <Typography variant="h4">
+                Custom Software Development
+              </Typography>
+              <Typography variant="subtitle1" className={classes.subtitle}>
+                  Save Energy. Save Time. Save Money.
+                </Typography>
+                <Typography variant="subtitle1">
+                  Complete digital solutions, from investigation to{" "} 
+                  <span className={classes.specialFont}>celebrate</span>
+              </Typography>
+              <Button variant="outlined" className={classes.learnButtonServices}>
+                  <span style={{marginRight: 10}}>Learn More</span>
+                  <ButtonArrow width={10} height={10} fill={theme.palette.common.blue}/>
+              </Button>
+            </Grid>
+            <Grid item>
+              <img className={classes.customSoftwareIcon} alt="custom software icon" src={customSoftwareIcon} />
             </Grid>
           </Grid>
         </Grid>
--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -2,6 +2,7 @@ import { createTheme } from "@material-ui/core";
 
 const arcBlue = '#0B72B9';
 const arcOrange = '#FFBA60';
+const arcGrey = '#868686';
 
 export default createTheme({
     palette: {
@@ -47,6 +48,20 @@ export default createTheme({
             fontSize: "1.75rem",
             color: `${arcBlue}`,
             fontWeight: 700
+        },
+        subtitle1: {
+            fontSize: "1.25rem",
+            fontWeight: 300,
+            color: `${arcGrey}`
+        },
+        learnButton: {
+            borderColor: arcBlue,
+            borderWidth: 2,
+            textTransform: "none",
+            color: arcBlue,
+            borderRadius: 50,
+            fontFamily: "Roboto",
+            fontWeight: "bold",
         }
     }
 });
