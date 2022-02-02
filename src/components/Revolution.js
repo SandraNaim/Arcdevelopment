aee3afb00f6eaf96c7c331f5128103919f527d78
Sandra Naim <naimsandra9@gmail.com>
2022-02-02 13:52:32 +0200

    create the revolution page

--- src/components/Revolution.js
+++ src/components/Revolution.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Lottie from "react-lottie";
 
 import { makeStyles, useTheme } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
@@ -10,9 +11,39 @@ import Typography from "@material-ui/core/Typography";
 
 import CallToAction from "./ui/CallToAction";
 
+import vision from "../assets/vision.svg";
+import technologyAnimation from "../animations/technologyAnimation/data.json";
+
 const useStyles = makeStyles(theme =>({
-    
+    rowContainer: {
+        paddingLeft: "5em",
+        paddingRight: "5em",
+        [theme.breakpoints.down("sm")]: {
+            paddingLeft: "1.5em",
+            paddingRight: "1.5em",
+        }
+    },
+    visionIcon: {
+        paddingRight: "3em",
+        width: "40em",
+        [theme.breakpoints.down("md")]: {
+            paddingRight: 0,
+            width: "100%"
+        }
+    },
+    subTitle: {
+        marginBottom: "0.5em"
+    }
 }))
+
+const technologyOptions = { 
+    loop: true,
+    autoplay: true, 
+    animationData: technologyAnimation,
+    rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice'
+    }
+}
 
 export default function Revolution(props) {
     const classes = useStyles();
@@ -23,10 +54,85 @@ export default function Revolution(props) {
 
     return(
         <Grid container direction="column">
+
+            {/* ___heading___ */}
             <Grid item style={{marginLeft: matchesSM ? 0 : "5em", marginTop: matchesSM ? "1em" : "2em"}}>
                 <Typography variant="h3" gutterBottom align={matchesSM ? "center" : undefined}>
                     The Revolution
                 </Typography>
+            </Grid>
+
+            {/* ___vision___ */}
+            <Grid item container direction={matchesMD ? "column" : "row"} alignItems="center" style={{marginTop: "10em"}} className={classes.rowContainer}>
+                <Grid item md>
+                    <img alt="visionIcon" src={vision} className={classes.visionIcon} />
+                </Grid>
+                <Grid item md align={matchesMD ? "center" : "right"}>
+                    <Typography variant="h4" className={classes.subTitle}>
+                        Vision
+                    </Typography>
+                    <Typography variant="body1" paragraph>
+                        The rise of computers, and subsequently the Internet, has completely altered every aspect of human life. 
+                        This has increased our comfort, broadened our connections, and reshaped how we view the world.
+                    </Typography>
+                    <Typography variant="body1" paragraph>
+                        What once was confined to huge rooms and teams of engineers now resides in every single one of our hands. 
+                        Harnessing this unlimited potential by using it to solve problems and better lives is at the heart of everything we do.
+                    </Typography>
+                    <Typography variant="body1" paragraph>
+                        We want to help businesses capitalize on the latest and greatest technology. The best way to predict the future is to be 
+                        the one building it, and we want to help guide the world into this next chapter of technological expansion, exploration, 
+                        and innovation.
+                    </Typography>
+                    <Typography variant="body1" paragraph>
+                        By holding ourselves to rigorous standards and pristine quality, we can ensure you have the absolute best tools 
+                        necessary to thrive in this new frontier.
+                    </Typography>
+                    <Typography variant="body1" paragraph>
+                        We see a future where every individual has personalized software custom tailored to their lifestyle, culture, 
+                        and interests, helping them overcome life's obstacles. Each project is a step towards that goal.
+                    </Typography>
+                </Grid>
+            </Grid>
+
+            {/* ___technology___ */}
+            <Grid item container direction="row" alignItems="center" style={{marginTop: "10em"}} className={classes.rowContainer}>
+                <Grid item md align={matchesMD ? "center" : undefined}>
+                    <Typography variant="h4" className={classes.subTitle}>
+                        Technology
+                    </Typography>
+                    <Typography variant="body1" paragraph>
+                        In 2013, Facebook invented a new way of building 
+                        websites. This new system, React.js, completely revolutionizes the process and practice of 
+                        website development.
+                    </Typography>
+                    <Typography variant="body1" paragraph>
+                        Instead of chaining together long individual pages, like traditional websites, React websites are built with 
+                        little chunks of code called components. 
+                        These components are faster, easier to maintain, and are easily reused and customized, each serving a singular purpose.
+                    </Typography>
+                    <Typography variant="body1" paragraph>
+                    Two years later they shocked the world by releasing a similar system, React Native, for producing iOS and Android apps. 
+                    Instead of having to master two completely separate development platforms, you can leverage the knowledge you already possessed 
+                    from building websites and reapply it directly! This was a huge leap forward.
+                    </Typography>
+                    <Typography variant="body1" paragraph>
+                        This technology is now being used by companies like AirBnB, Microsoft, Netflix, Pinterest, Skype, Tesla, UberEats, and when Facebook 
+                        purchased Instagram large portions of it were even rebuilt using React.
+                    </Typography>
+                    <Typography variant="body1" paragraph>
+                        Developers have since built on top of these systems by automating project setup and deployment, 
+                        allowing creators to focus as much as possible on their work itself.
+                    </Typography>
+                    <Typography variant="body1" paragraph>
+                        These technical advancements translate into savings by significantly reducing the workload and 
+                        streamlining the workflow for developing new pieces of software, while also lowering the barrier to entry 
+                        for mobile app development. This puts personalization in your pocket — faster, better, and more affordable than ever before.
+                    </Typography>
+                </Grid>
+                <Grid item md>
+                    <Lottie options={technologyOptions} />
+                </Grid>
             </Grid>
 
             {/* ___call to action___ */}
